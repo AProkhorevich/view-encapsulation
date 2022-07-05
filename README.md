091bbe9be71663bd1708934d571972847a2149df
AProkhorevich <a.prohorevich@mail.ru>
2022-07-05 11:29:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gf9x79-hsihkv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gf9x79-hsihkv)
---
+++ src/app/emulated-encapsulation.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-emulated-encapsulation',
+  template: `
+    <h2>Emulated</h2>
+    <div class="emulated-message">Emulated encapsulation</div>
+    <app-no-encapsulation></app-no-encapsulation>
+    <div class="none-message">No encapsulation</div>
+    <div class="shadow-message">Shadow DOM encapsulation</div>
+
+  `,
+  styles: ['h2, .emulated-message { color: green; }'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class EmulatedEncapsulationComponent {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
